114c7a51cad9d0885d2e1216a66b55c0b73572f7
seojaemin <seojaemin@seojaemin-ui-MacBookPro.local>
2023-07-07 13:21:15 +0900

    first commit

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,33 @@
+import { prisma } from '@/app/lib/server';
+import { NextResponse } from 'next/server';
+
+export const POST = async (req: Request) => {
+  try {
+    const { account, email, signedToken } = await req.json();
+
+    const user = await prisma.user.upsert({
+      //업데이트,크리에이트를 같이 사용할수있음
+      where: { account },
+      update: { signedToken },
+      create: {
+        account,
+        email,
+        signedToken,
+      },
+      select: {
+        account: true,
+        email: true,
+        nickname: true,
+        signedToken: true,
+      },
+    });
+
+    // console.log('Work!!!');
+    return NextResponse.json({
+      ok: true,
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
